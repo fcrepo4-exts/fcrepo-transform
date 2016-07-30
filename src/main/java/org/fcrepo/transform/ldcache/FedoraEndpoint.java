83cce7c3a7e24e287481cab582ec2463de79368d
Aaron Coburn <acoburn@amherst.edu>
2016-07-30 13:43:29 -0400

    update javadocs

--- src/main/java/org/fcrepo/transform/ldcache/FedoraEndpoint.java
+++ src/main/java/org/fcrepo/transform/ldcache/FedoraEndpoint.java
@@ -26,20 +26,28 @@ import org.apache.marmotta.ldclient.api.endpoint.Endpoint;
 public class FedoraEndpoint extends Endpoint {
 
     /**
-     * Create a Fedora endpoint
-     * @param baseUrl the Base URL of the Fedora endpoint
+     * Create a Fedora endpoint to be used with the Marmotta LDClient
+     * Note: the baseUrl defined here is used by the LDClient to determine
+     * what rules to apply when dereferencing URIs that start with the
+     * given namespace.
+     *
+     * @param namespace the namespace for the Fedora endpoint (e.g. http://localhost:8080/fcrepo/rest/)
      */
-    public FedoraEndpoint(final String baseUrl) {
-        this(baseUrl, 0L);
+    public FedoraEndpoint(final String namespace) {
+        this(namespace, 0L);
     }
 
     /**
-     * Create a Fedora endpoint
-     * @param baseUrl the Base URL of the Fedora endpoint
+     * Create a Fedora endpoint to be used with the Marmotta LDClient
+     * Note: the baseUrl defined here is used by the LDClient to determine
+     * what rules to apply when dereferencing URIs that start with the
+     * given namespace.
+     *
+     * @param namespace the namespace for the Fedora endpoint (e.g. http://localhost:8080/fcrepo/rest/)
      * @param timeout the length of time (in seconds) to cache the data
      */
-    public FedoraEndpoint(final String baseUrl, final long timeout) {
-        super("Fedora Repository", "Linked Data", baseUrl + ".*", null, timeout);
+    public FedoraEndpoint(final String namespace, final long timeout) {
+        super("Fedora Repository", "Linked Data", namespace + ".*", null, timeout);
         setPriority(PRIORITY_HIGH);
         addContentType(new ContentType("application", "rdf+xml", 0.8));
         addContentType(new ContentType("text", "turtle", 1.0));
